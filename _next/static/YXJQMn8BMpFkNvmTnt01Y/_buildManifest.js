6cd1d29ccac01a2af6f0263351e64e5165eef015
eni4sure <eni4sure@users.noreply.github.com>
2021-12-14 19:47:16 +0000

    Deploying to gh-pages from @ GenZTechies/gzt-web@d92eb86ea0e179369b634f2ac3d3818a2d500f14 🚀

---
+++ _next/static/YXJQMn8BMpFkNvmTnt01Y/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7fb2612bc4160131.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
