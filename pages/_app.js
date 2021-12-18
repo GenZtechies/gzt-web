1bd7841ed06cb15f76be577890bc15291b40ce14
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-12-18 19:55:25 +0100

    first two sections of home page built and responsive, some minor issues on the largedt screen(4k screen) will visit, others can go ahead now

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
