4de5b3c61a45301587537bd30b933eca273ebb2a
Eniola Osabiya <eosabiya@gmail.com>
2021-12-23 13:32:52 +0100

    members CLI util complete

---
+++ members/index.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    "name": "Eniola",
+    "image_url": "https://avatars.githubusercontent.com/u/52782730?v=4",
+    "short_description": "",
+    "role": "Full Stack Developer",
+    "links": {
+      "website": "https://eniolaosabiya.com",
+      "github": "https://github.com/eni4sure",
+      "twitter": "https://github.com/eni4sure",
+      "linkedin": "https://www.linkedin.com/in/eniola-osabiya"
+    },
+    "location": {
+      "state": "Lagos",
+      "country": "Nigeria"
+    }
+  }
+];
