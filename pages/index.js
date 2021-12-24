35e90486858b71d5e5e22e62238b68b6c9566832
Adetunji Adeyinka <55273527+Itzadetunji@users.noreply.github.com>
2021-12-24 17:28:22 +0100

    Adjusted some positions

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Navbar, Footer } from "../components";
-
 import { gsap } from "gsap";
 
 function Index() {
