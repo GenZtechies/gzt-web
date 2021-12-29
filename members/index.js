6afa32eed8493559f442a2d0d7445856e2ca2761
Eniola Osabiya <eosabiya@gmail.com>
2021-12-30 00:31:50 +0100

    members update

--- members/index.js
+++ members/index.js
@@ -18,6 +18,41 @@ module.exports = [
     "isActive": true
   },
   {
+    "full_name": "Ikram Babs-Lawal",
+    "username": "Code_Quinn",
+    "image_url": "https://scontent-lga3-2.xx.fbcdn.net/v/t39.30808-6/fr/cp0/e15/q65/241872053_388537542797135_3797128416203328467_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=7aed08&efg=eyJpIjoidCJ9&_nc_ohc=hqquoe3LEXYAX__VpAG&_nc_ht=scontent-lga3-2.xx&oh=00_AT_su8DpbAQ2WIWIGj_bfckRZx5-_B7J6s4vLpyAVvNOIw&oe=61D2BCDD",
+    "short_description": "I started my coding journey when I was 11yrs Old. I am obsessed with cats, chocolate and coding 😊",
+    "role": "Frontend Developer",
+    "links": {
+      "github": "https://github.com/hikky08",
+      "twitter": "https://twitter.com/code_quinn",
+      "linkedin": "https://www.linkedin.com/in/ikram-babs-lawal"
+    },
+    "location": {
+      "state": "Lagos",
+      "country": "Nigeria"
+    },
+    "isActive": true
+  },
+  {
+    "full_name": "Okpe Onoja",
+    "username": "onoja123",
+    "image_url": "https://cdn.hashnode.com/res/hashnode/image/upload/v1599345517785/CTo_08ERw.jpeg?w=400&h=400&fit=crop&crop=faces&auto=compress",
+    "short_description": "A 16 years old Fullstack developer based in Nigeria. I strive to develop new products that make life easier and flexible😊",
+    "role": "Full Stack Developer",
+    "links": {
+      "website_or_extra": "https://okpeonoja.netlify.app/",
+      "github": "https://github.com/onoja123",
+      "twitter": "https://twitter.com/okpe_onoja",
+      "linkedin": "https://www.linkedin.com/in/okpe-godwin-onoja-5814311b5/"
+    },
+    "location": {
+      "state": "Kaduna",
+      "country": "Nigeria"
+    },
+    "isActive": true
+  },
+  {
     "full_name": "Ilyas Rufai",
     "username": "rufilboy",
     "image_url": "https://avatars.githubusercontent.com/u/69714197?v=4",
@@ -31,6 +66,24 @@ module.exports = [
     },
     "location": {
       "state": "Ibadan",
+      "country": "Nigeria"
+    },
+    "isActive": true
+  },
+  {
+    "full_name": "John Olatubosun",
+    "username": "toluolatubosun",
+    "image_url": "https://avatars.githubusercontent.com/u/64479657?v=4",
+    "short_description": "I enjoy working on new project ideas. Also I am a crypto enthusiast 😎",
+    "role": "Full Stack Developer",
+    "links": {
+      "website_or_extra": "https://n-timee.web.app/",
+      "github": "https://github.com/toluolatubosun",
+      "twitter": "https://twitter.com/king_tolu_7",
+      "linkedin": "https://www.linkedin.com/in/john-olatubosun/"
+    },
+    "location": {
+      "state": "Lagos",
       "country": "Nigeria"
     },
     "isActive": true
