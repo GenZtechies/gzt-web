2d276a73e15d6dfc0ec7f7275030819a01182887
Ronald Dosunmu <ronaldosunmu@gmail.com>
2021-12-29 17:26:54 +0000

    members update

--- members/index.js
+++ members/index.js
@@ -18,6 +18,59 @@ module.exports = [
     "isActive": true
   },
   {
+    "full_name": "Ronald Dosunmu",
+    "username": "Iamronaldosunmu",
+    "image_url": "https://media-exp1.licdn.com/dms/image/C4D03AQHrZzvVSGQaHA/profile-displayphoto-shrink_800_800/0/1636403300127?e=1646265600&v=beta&t=H4Uv7F9L1CawXWtnfuwHoDqJE5BEMBGSay1oEHWv0J4",
+    "short_description": "I started my coding journey in 2018. I love building beautiful web applications that solve problems.",
+    "role": "Full Stack Developer",
+    "links": {
+      "website_or_extra": "",
+      "github": "https://github.com/Iamronaldosunmu",
+      "twitter": "https://www.twitter.com/IamRonald05",
+      "linkedin": "https://www.linkedin.com/in/ronald-dosunmu-2b3a77202/"
+    },
+    "location": {
+      "state": "Lagos",
+      "country": "Nigeria"
+    },
+    "isActive": true
+  },
+  {
+    "full_name": "Ikram Babs-Lawal",
+    "username": "Code_Quinn",
+    "image_url": "https://m.facebook.com/photo.php?fbid=388537539463802&id=100049226779149&set=a.216211910029700&source=11&refid=17&_ft_=mf_story_key.388537582797131%3Atop_level_post_id.388537539463802%3Atl_objid.388537539463802%3Acontent_owner_id_new.100049226779149%3Athrowback_story_fbid.388537582797131%3Astory_location.4%3Astory_attachment_style.profile_media%3Athid.100049226779149%3A306061129499414%3A30%3A0%3A1641023999%3A-4693220578658978736%3A%3A&__tn__=H-R",
+    "short_description": "I started my coding journey when I was 11yrs Old. I am obsessed with cats, chocolate and coding 😊",
+    "role": "Frontend Developer",
+    "links": {
+      "github": "https://github.com/hikky08",
+      "twitter": "https://twitter.com/code_quinn",
+      "linkedin": "https://www.linkedin.com/in/ikram-babs-lawal"
+    },
+    "location": {
+      "state": "Lagos",
+      "country": "Nigeria"
+    },
+    "isActive": true
+  },
+  {
+    "full_name": "Okpe Onoja",
+    "username": "onoja123",
+    "image_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fhashnode.com%2F%40Iam_the_code&psig=AOvVaw1mOIVyYxd6rmQdOxYrnFOF&ust=1640420878037000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJCNnLWH_PQCFQAAAAAdAAAAABAD",
+    "short_description": "A 16 years old Fullstack developer based in Nigeria. I strive to develop new products that make life easier and flexible😊",
+    "role": "Full Stack Developer",
+    "links": {
+      "website_or_extra": "https://okpeonoja.netlify.app/",
+      "github": "https://github.com/onoja123",
+      "twitter": "https://twitter.com/okpe_onoja",
+      "linkedin": "https://www.linkedin.com/in/okpe-godwin-onoja-5814311b5/"
+    },
+    "location": {
+      "state": "Kaduna",
+      "country": "Nigeria"
+    },
+    "isActive": true
+  },
+  {
     "full_name": "Ilyas Rufai",
     "username": "rufilboy",
     "image_url": "https://avatars.githubusercontent.com/u/69714197?v=4",
@@ -31,6 +84,24 @@ module.exports = [
     },
     "location": {
       "state": "Ibadan",
+      "country": "Nigeria"
+    },
+    "isActive": true
+  },
+  {
+    "full_name": "John Olatubosun",
+    "username": "toluolatubosun",
+    "image_url": "https://avatars.githubusercontent.com/u/64479657?v=4",
+    "short_description": "I enjoy working on new project ideas. Also I am a crypto enthusiast 😎",
+    "role": "Full Stack Developer",
+    "links": {
+      "website_or_extra": "https://n-timee.web.app/",
+      "github": "https://github.com/toluolatubosun",
+      "twitter": "https://twitter.com/king_tolu_7",
+      "linkedin": "https://www.linkedin.com/in/john-olatubosun/"
+    },
+    "location": {
+      "state": "Lagos",
       "country": "Nigeria"
     },
     "isActive": true
