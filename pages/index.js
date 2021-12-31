5877118189c31b56f8481b68dea811da2dbd6a46
caleb-007 <calebareeveso7@gmail.com>
2021-12-31 18:42:31 +0100

    feat: wrapping pages with layout tag

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Navbar, Footer, Layout } from "../components";
+import { Layout } from "../components";
 
 function Index() {
   return (
