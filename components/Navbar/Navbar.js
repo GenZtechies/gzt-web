b5bc6a44368c90aaa07638ce5ef129157be83f47
caleb-007 <calebareeveso7@gmail.com>
2021-12-31 18:07:54 +0100

    style: made button interactive

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -64,7 +64,10 @@ function Navbar() {
                 target="_blank"
                 className="text-[#3331C4] hidden md:block"
               >
-                <button className="p-3 bg-yellow-500 rounded-full">
+                <button
+                  className="p-3 bg-yellow-500 rounded-full hover:scale-105 transition duration-150 ease-in-out
+                "
+                >
                   Join The Community
                 </button>
               </a>
