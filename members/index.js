79958ce1175d2ea0c55c08072fbf43c48fefc2d0
caleb-007 <calebareeveso7@gmail.com>
2021-12-31 16:09:29 +0100

    chore: made changes to my member JSON

--- members/index.js
+++ members/index.js
@@ -1,137 +1,127 @@
 module.exports = [
   {
-    full_name: "Caleb Areeveso",
-    username: "calebareeveso",
-    image_url: "https://avatars.githubusercontent.com/u/58891908?v=4",
-    short_description:
-      "I build and provide innovative Solutions that transform businesses and lives",
-    role: "Full Stack Developer",
-    links: {
-      website_or_extra: "https://linktr.ee/calebareeveso",
-      github: "https://github.com/calebareeveso",
-      twitter: "https://www.twitter.com/calebareeveso",
-      linkedin: "https://www.linkedin.com/in/calebareeveso"
+    "full_name": "Caleb Areeveso",
+    "username": "calebareeveso",
+    "image_url": "https://avatars.githubusercontent.com/u/58891908?v=4",
+    "short_description": "I build and provide innovative Solutions that transform businesses and lives",
+    "role": "Full Stack Developer",
+    "links": {
+      "website_or_extra": "https://linktr.ee/calebareeveso",
+      "github": "https://github.com/calebareeveso",
+      "twitter": "https://www.twitter.com/calebareeveso",
+      "linkedin": "https://www.linkedin.com/in/calebareeveso"
     },
-    location: {
-      state: "Lgos",
-      country: "Nigeria"
+    "location": {
+      "state": "Lgos",
+      "country": "Nigeria"
     },
-    isActive: true
+    "isActive": true
   },
   {
-    full_name: "Eniola Osabiya",
-    username: "eni4sure",
-    image_url: "https://avatars.githubusercontent.com/u/52782730?v=4",
-    short_description:
-      "I started my coding journey in 2016, when I was 12yrs Old. I have an obsession for *clean* and structured code 😊",
-    role: "Full Stack Developer",
-    links: {
-      website_or_extra: "https://eniolaosabiya.com",
-      github: "https://github.com/eni4sure",
-      twitter: "https://twitter.com/eni4sure",
-      linkedin: "https://www.linkedin.com/in/eniola-osabiya"
+    "full_name": "Eniola Osabiya",
+    "username": "eni4sure",
+    "image_url": "https://avatars.githubusercontent.com/u/52782730?v=4",
+    "short_description": "I started my coding journey in 2016, when I was 12yrs Old. I have an obsession for *clean* and structured code 😊",
+    "role": "Full Stack Developer",
+    "links": {
+      "website_or_extra": "https://eniolaosabiya.com",
+      "github": "https://github.com/eni4sure",
+      "twitter": "https://twitter.com/eni4sure",
+      "linkedin": "https://www.linkedin.com/in/eniola-osabiya"
     },
-    location: {
-      state: "Lagos",
-      country: "Nigeria"
+    "location": {
+      "state": "Lagos",
+      "country": "Nigeria"
     },
-    isActive: true
+    "isActive": true
   },
   {
-    full_name: "Ronald Dosunmu",
-    username: "Iamronaldosunmu",
-    image_url:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQHrZzvVSGQaHA/profile-displayphoto-shrink_800_800/0/1636403300127?e=1646265600&v=beta&t=H4Uv7F9L1CawXWtnfuwHoDqJE5BEMBGSay1oEHWv0J4",
-    short_description:
-      "I started my coding journey in 2018. I love building beautiful web applications that solve problems.",
-    role: "Full Stack Developer",
-    links: {
-      website_or_extra: "",
-      github: "https://github.com/Iamronaldosunmu",
-      twitter: "https://www.twitter.com/IamRonald05",
-      linkedin: "https://www.linkedin.com/in/ronald-dosunmu-2b3a77202/"
+    "full_name": "Ronald Dosunmu",
+    "username": "Iamronaldosunmu",
+    "image_url": "https://media-exp1.licdn.com/dms/image/C4D03AQHrZzvVSGQaHA/profile-displayphoto-shrink_800_800/0/1636403300127?e=1646265600&v=beta&t=H4Uv7F9L1CawXWtnfuwHoDqJE5BEMBGSay1oEHWv0J4",
+    "short_description": "I started my coding journey in 2018. I love building beautiful web applications that solve problems.",
+    "role": "Full Stack Developer",
+    "links": {
+      "website_or_extra": "",
+      "github": "https://github.com/Iamronaldosunmu",
+      "twitter": "https://www.twitter.com/IamRonald05",
+      "linkedin": "https://www.linkedin.com/in/ronald-dosunmu-2b3a77202/"
     },
-    location: {
-      state: "Lagos",
-      country: "Nigeria"
+    "location": {
+      "state": "Lagos",
+      "country": "Nigeria"
     },
-    isActive: true
+    "isActive": true
   },
   {
-    full_name: "Ikram Babs-Lawal",
-    username: "Code_Quinn",
-    image_url:
-      "https://scontent-lga3-2.xx.fbcdn.net/v/t39.30808-6/fr/cp0/e15/q65/241872053_388537542797135_3797128416203328467_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=7aed08&efg=eyJpIjoidCJ9&_nc_ohc=hqquoe3LEXYAX__VpAG&_nc_ht=scontent-lga3-2.xx&oh=00_AT_su8DpbAQ2WIWIGj_bfckRZx5-_B7J6s4vLpyAVvNOIw&oe=61D2BCDD",
-    short_description:
-      "I started my coding journey when I was 11yrs Old. I am obsessed with cats, chocolate and coding 😊",
-    role: "Frontend Developer",
-    links: {
-      github: "https://github.com/hikky08",
-      twitter: "https://twitter.com/code_quinn",
-      linkedin: "https://www.linkedin.com/in/ikram-babs-lawal"
+    "full_name": "Ikram Babs-Lawal",
+    "username": "Code_Quinn",
+    "image_url": "https://scontent-lga3-2.xx.fbcdn.net/v/t39.30808-6/fr/cp0/e15/q65/241872053_388537542797135_3797128416203328467_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=7aed08&efg=eyJpIjoidCJ9&_nc_ohc=hqquoe3LEXYAX__VpAG&_nc_ht=scontent-lga3-2.xx&oh=00_AT_su8DpbAQ2WIWIGj_bfckRZx5-_B7J6s4vLpyAVvNOIw&oe=61D2BCDD",
+    "short_description": "I started my coding journey when I was 11yrs Old. I am obsessed with cats, chocolate and coding 😊",
+    "role": "Frontend Developer",
+    "links": {
+      "github": "https://github.com/hikky08",
+      "twitter": "https://twitter.com/code_quinn",
+      "linkedin": "https://www.linkedin.com/in/ikram-babs-lawal"
     },
-    location: {
-      state: "Lagos",
-      country: "Nigeria"
+    "location": {
+      "state": "Lagos",
+      "country": "Nigeria"
     },
-    isActive: true
+    "isActive": true
   },
   {
-    full_name: "Okpe Onoja",
-    username: "onoja123",
-    image_url:
-      "https://cdn.hashnode.com/res/hashnode/image/upload/v1599345517785/CTo_08ERw.jpeg?w=400&h=400&fit=crop&crop=faces&auto=compress",
-    short_description:
-      "A 16 years old Fullstack developer based in Nigeria. I strive to develop new products that make life easier and flexible😊",
-    role: "Full Stack Developer",
-    links: {
-      website_or_extra: "https://okpeonoja.netlify.app/",
-      github: "https://github.com/onoja123",
-      twitter: "https://twitter.com/okpe_onoja",
-      linkedin: "https://www.linkedin.com/in/okpe-godwin-onoja-5814311b5/"
+    "full_name": "Okpe Onoja",
+    "username": "onoja123",
+    "image_url": "https://cdn.hashnode.com/res/hashnode/image/upload/v1599345517785/CTo_08ERw.jpeg?w=400&h=400&fit=crop&crop=faces&auto=compress",
+    "short_description": "A 16 years old Fullstack developer based in Nigeria. I strive to develop new products that make life easier and flexible😊",
+    "role": "Full Stack Developer",
+    "links": {
+      "website_or_extra": "https://okpeonoja.netlify.app/",
+      "github": "https://github.com/onoja123",
+      "twitter": "https://twitter.com/okpe_onoja",
+      "linkedin": "https://www.linkedin.com/in/okpe-godwin-onoja-5814311b5/"
     },
-    location: {
-      state: "Kaduna",
-      country: "Nigeria"
+    "location": {
+      "state": "Kaduna",
+      "country": "Nigeria"
     },
-    isActive: true
+    "isActive": true
   },
   {
-    full_name: "Ilyas Rufai",
-    username: "rufilboy",
-    image_url: "https://avatars.githubusercontent.com/u/69714197?v=4",
-    short_description:
-      "I started my coding journey in 2019, I've in view of helping big companies and startups deliver better software.",
-    role: "DevOps Engineer",
-    links: {
-      website_or_extra: "https://github.com/rufilboy",
-      github: "https://github.com/rufilboy",
-      twitter: "https://twitter.com/IlyasRufai",
-      linkedin: "https://www.linkedin.com/in/ilyas-rufai"
+    "full_name": "Ilyas Rufai",
+    "username": "rufilboy",
+    "image_url": "https://avatars.githubusercontent.com/u/69714197?v=4",
+    "short_description": "I started my coding journey in 2019, I've in view of helping big companies and startups deliver better software.",
+    "role": "DevOps Engineer",
+    "links": {
+      "website_or_extra": "https://github.com/rufilboy",
+      "github": "https://github.com/rufilboy",
+      "twitter": "https://twitter.com/IlyasRufai",
+      "linkedin": "https://www.linkedin.com/in/ilyas-rufai"
     },
-    location: {
-      state: "Ibadan",
-      country: "Nigeria"
+    "location": {
+      "state": "Ibadan",
+      "country": "Nigeria"
     },
-    isActive: true
+    "isActive": true
   },
   {
-    full_name: "John Olatubosun",
-    username: "toluolatubosun",
-    image_url: "https://avatars.githubusercontent.com/u/64479657?v=4",
-    short_description:
-      "I enjoy working on new project ideas. Also I am a crypto enthusiast 😎",
-    role: "Full Stack Developer",
-    links: {
-      website_or_extra: "https://n-timee.web.app/",
-      github: "https://github.com/toluolatubosun",
-      twitter: "https://twitter.com/king_tolu_7",
-      linkedin: "https://www.linkedin.com/in/john-olatubosun/"
+    "full_name": "John Olatubosun",
+    "username": "toluolatubosun",
+    "image_url": "https://avatars.githubusercontent.com/u/64479657?v=4",
+    "short_description": "I enjoy working on new project ideas. Also I am a crypto enthusiast 😎",
+    "role": "Full Stack Developer",
+    "links": {
+      "website_or_extra": "https://n-timee.web.app/",
+      "github": "https://github.com/toluolatubosun",
+      "twitter": "https://twitter.com/king_tolu_7",
+      "linkedin": "https://www.linkedin.com/in/john-olatubosun/"
     },
-    location: {
-      state: "Lagos",
-      country: "Nigeria"
+    "location": {
+      "state": "Lagos",
+      "country": "Nigeria"
     },
-    isActive: true
+    "isActive": true
   }
 ];
