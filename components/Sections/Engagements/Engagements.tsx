4a746a03b15ba9771f5d142f6a9e5d3f7613565b
Usman-Hassan-Gu <usmanhassangu@gmail.com>
2022-09-02 22:28:01 +0100

    feat: implements engagements texts

--- components/Sections/Engagements/Engagements.tsx
+++ components/Sections/Engagements/Engagements.tsx
@@ -13,14 +13,14 @@ const Engagements = () => {
             <div className="flex items-start space-x-4">
                 <img src="/images/hackathon.png" alt="events" className="w-20 " />
                 <div>
-                    <h1 className="text-xl font-bold">Events and Partnerships</h1>
+                    <h1 className="text-xl font-bold">Hackathons</h1>
                     <p className="text-gray-400 text-sm">A website that brings together teen developers and innovators from around Nigeria in one place.</p>
                 </div>
             </div>
             <div className="flex items-start space-x-4">
                 <img src="/images/hangouts.png" alt="events" className="w-20 " />
                 <div>
-                    <h1 className="text-xl font-bold">Events and Partnerships</h1>
+                    <h1 className="text-xl font-bold">Hangouts and Fun</h1>
                     <p className="text-gray-400 text-sm">A website that brings together teen developers and innovators from around Nigeria in one place.</p>
                 </div>
             </div>
