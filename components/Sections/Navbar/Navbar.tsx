aae957743e69f175bbadc9f101f11ef6ed248b1b
Usman Hassan <usmanhassangu@gmail.com>
2022-09-12 18:14:08 +0100

    chore: make nav links capitalized

--- components/Sections/Navbar/Navbar.tsx
+++ components/Sections/Navbar/Navbar.tsx
@@ -27,14 +27,14 @@ const Navbar = () => {
                     </a>
                 </Link>
             </div>
-            <ul className="hidden md:flex items-center space-x-1">
+            <ul className="hidden md:flex items-center">
                 {NavLinks.map((link: NavLinksType, index: number) => (
                     <li key={link.title}>
                         <Link href={link.url}>
                             <a>
-                                <button className="no-animation btn btn-ghost hover:bg-transparent gap-2">
+                                <button className="no-animation btn btn-ghost hover:bg-transparent gap-2 capitalize">
                                     <>
-                                        {link.title}{" "}
+                                        {link.title}
                                         {link.drop ? (
                                             <svg width="14" height="8" viewBox="0 0 14 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M1.5 1.25L7 6.75L12.5 1.25" stroke="#5D5CD6" strokeWidth="2.4" strokeLinecap="round" strokeLinejoin="round" />
