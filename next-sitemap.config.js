5064c96ef8763cb9b1a88d5ded57045100bfdc85
Eniola Osabiya <eosabiya@gmail.com>
2023-04-25 17:14:45 -0500

    chore: minor update to sitemap generator

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || "https://www.genztechies.com/",
+    siteUrl: "https://www.genztechies.com/",
     changefreq: "monthly",
-    generateRobotsTxt: true, // (optional)
+    generateRobotsTxt: true,
     generateIndexSitemap: false
 };
